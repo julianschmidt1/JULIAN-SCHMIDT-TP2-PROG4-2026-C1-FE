a2f051578d974daf418d412c4ddaf979090562e9
Julian Schmidt <juli99nic@gmail.com>
2026-06-25 19:37:09 -0300

    Like - Dislike implementation in post detail screen. Working comment edition

--- src/app/features/posts/post-detail/post-detail.ts
+++ src/app/features/posts/post-detail/post-detail.ts
@@ -9,10 +9,11 @@ import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { AuthStorageService } from '../../../core/services/auth-storage';
 import { CommentsService } from '../../comments/comments.service';
 import { CommentResponse } from '../../comments/models/comment-response';
+import { ButtonDirective } from 'primeng/button';
 
 @Component({
   selector: 'app-post-detail',
-  imports: [DatePipe, RouterLink, ReactiveFormsModule],
+  imports: [DatePipe, RouterLink, ReactiveFormsModule, ButtonDirective],
   templateUrl: './post-detail.html',
   styleUrl: './post-detail.scss',
 })
@@ -25,15 +26,17 @@ export class PostDetail implements OnInit {
   private readonly fb = inject(FormBuilder);
   private readonly commentsService = inject(CommentsService);
   private readonly authStorage = inject(AuthStorageService);
-
   readonly currentUser = this.authStorage.getUser();
-
   readonly comments = signal<CommentResponse[]>([]);
   readonly hasMoreComments = signal(true);
-
   readonly commentsLimit = 5;
-
   readonly commentForm = this.fb.nonNullable.group({
+    message: ['', [Validators.required, Validators.maxLength(500)]],
+  });
+
+  readonly editingCommentId = signal<string | null>(null);
+
+  readonly editCommentForm = this.fb.nonNullable.group({
     message: ['', [Validators.required, Validators.maxLength(500)]],
   });
 
@@ -123,5 +126,62 @@ export class PostDetail implements OnInit {
           this.commentForm.reset();
         },
       });
+  }
+
+  canEditComment(comment: CommentResponse): boolean {
+    return this.currentUser?.id === comment.author.id;
+  }
+
+  startEditComment(comment: CommentResponse): void {
+    this.editingCommentId.set(comment.id);
+    this.editCommentForm.setValue({
+      message: comment.message,
+    });
+  }
+
+  cancelEditComment(): void {
+    this.editingCommentId.set(null);
+    this.editCommentForm.reset();
+  }
+
+  onLikeClicked(): void {
+    const currentPost = this.post();
+
+    if (!currentPost) {
+      return;
+    }
+
+    const request$ = currentPost.likedByCurrentUser
+      ? this.postsService.unlike(currentPost.id)
+      : this.postsService.like(currentPost.id);
+
+    request$.subscribe({
+      next: (updatedPost) => {
+        this.post.set(updatedPost);
+      },
+    });
+  }
+
+  saveEditedComment(comment: CommentResponse): void {
+    if (this.editCommentForm.invalid) {
+      this.editCommentForm.markAllAsTouched();
+      return;
+    }
+
+    const message = this.editCommentForm.getRawValue().message;
+
+    this.commentsService.update(comment.id, message).subscribe({
+      next: (updatedComment) => {
+        this.comments.update((comments) =>
+          comments.map((currentComment) =>
+            currentComment.id === updatedComment.id
+              ? updatedComment
+              : currentComment,
+          ),
+        );
+
+        this.cancelEditComment();
+      },
+    });
   }
 }
